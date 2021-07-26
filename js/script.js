234fa653bc5d717f25e9bf25b44a937d8c48cc8e
NicolasBQ <nicolasbquintero@gmail.com>
2021-07-26 12:46:34 -0500

    Ai vs Player

--- js/script.js
+++ js/script.js
@@ -231,20 +231,37 @@ function mount() {
    }
 //Player vs Machine
    if(document.querySelector('.score-machine-container') && !document.querySelector('.score-container')) {
-       const player = (sign) => {
-           this.sign = this.sign
-           const getSign = () => {
-               return sign
-           }
+    //    const player = () => {
+    //         let human = 'X';
+    //         let machine = 'O';
+    //         let currentPlayerM = human;
 
-           return{getSign}
+    //         const getCurrentPlayer = (player) => {
+    //             currentPlayerM = player;
+    //         }
 
-       }
+    //         const getHuman = () => {
+    //             return human;
+    //         }
+
+    //         const getMachine = () => {
+    //             return machine;
+    //         }
+
+    //         return{getCurrentPlayer, getHuman, getMachine};
+
+    //    }
+
 
        const gameboardM = (() => {
-           const _gameboardM = ["", "", "", "", "", "", "", ""];
-           let bestScore = -Infinity;
-           let bestMoveM;
+           const _gameboardM = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+           const fieldElementsM = document.querySelectorAll('.fieldM');
+           const humanScore = document.querySelector('.score-player');
+           const machineScore = document.querySelector('.score-machine');
+           const currentPlayer = document.querySelector('.current-player');
+           let huPlayer = 'O';
+           let aiPlayer = 'X';
+           let fc = 0;
 
            const setGameboardM = (index, sign) => {
             _gameboardM[index] = sign;
@@ -255,69 +272,238 @@ function mount() {
             return _gameboardM[index]
         }
 
+        const emptyIndexies = (board) => {
+            return board.filter(s => s != 'O' && s != 'X')
+        }
+        
         const bestMove = () => {
-            for(let i = 0; i < _gameboardM.length; i++) {
-                if(_gameboardM[i] == '') {
-                    let score = minimax(_gameboardM);
-                    if(score > bestScore) {
-                        bestScore = score;
-                        setGameboardM(i, 'O');
-                        displayControllerM.domGameboardM(i);
-                    }
+            let bestSpot = minimax(_gameboardM, aiPlayer);
+            setGameboardM(bestSpot.index, aiPlayer);
+            domGameboardM(bestSpot.index);
+            humanTurn();
+        }
+        
+        const minimax = (newBoard, player) => {
+            //add one to function calls
+            fc++;
+            
+            //available spots
+            let availSpots = emptyIndexies(newBoard);
+          
+            // checks for the terminal states such as win, lose, and tie and returning a value accordingly
+            if (winning(newBoard, huPlayer)){
+               return {score:-10};
+            }
+              else if (winning(newBoard, aiPlayer)){
+              return {score:10};
+              }
+            else if (availSpots.length === 0){
+                return {score:0};
+            }
+          
+          // an array to collect all the objects
+            let moves = [];
+          
+            // loop through available spots
+            for (let i = 0; i < availSpots.length; i++){
+              //create an object for each and store the index of that spot that was stored as a number in the object's index key
+              let move = {};
+                move.index = newBoard[availSpots[i]];
+          
+              // set the empty spot to the current player
+              newBoard[availSpots[i]] = player;
+          
+              //if collect the score resulted from calling minimax on the opponent of the current player
+              if (player == aiPlayer){
+                let result = minimax(newBoard, huPlayer);
+                move.score = result.score;
+              }
+              else{
+                let result = minimax(newBoard, aiPlayer);
+                move.score = result.score;
+              }
+          
+              //reset the spot to empty
+              newBoard[availSpots[i]] = move.index;
+          
+              // push the object to the array
+              moves.push(move);
+            }
+          
+          // if it is the computer's turn loop over the moves and choose the move with the highest score
+            let bestMove;
+            if(player === aiPlayer){
+              let bestScore = -10000;
+              for(let i = 0; i < moves.length; i++){
+                if(moves[i].score > bestScore){
+                  bestScore = moves[i].score;
+                  bestMove = i;
                 }
+              }
+            }else{
+          
+          // else loop over the moves and choose the move with the lowest score
+              let bestScore = 10000;
+              for(let i = 0; i < moves.length; i++){
+                if(moves[i].score < bestScore){
+                  bestScore = moves[i].score;
+                  bestMove = i;
+                }
+              }
             }
-        }
-
-        const minimax = (gameboard) =>{
-           return 1;
-        }
-
-        return{setGameboardM, getGameboardM, bestMove}
-
-       })();
-
-       const displayControllerM = (() => {
-           const fieldElementsM = document.querySelectorAll('.fieldM');
-
-           fieldElementsM.forEach((field) => {
-               field.addEventListener('click', (e) => {
-                   if(e.target.textContent != '')return;
-                    gameControllerM.playRound(parseInt(e.target.dataset.index));
-               })
-           })
-
-
-           const domGameboardM = (index) => {
-            fieldElementsM[index].textContent = gameboardM.getGameboardM(index);
-            if(gameboardM.getGameboardM(index) === 'X') {
-                fieldElementsM[index].style.color = '#39F01F'
+          
+          // return the chosen move (object) from the array to the higher depth
+            return moves[bestMove];
+          }
+        
+        
+        
+        const winning = (board, player) => {
+            if (
+                (board[0] == player && board[1] == player && board[2] == player) ||
+                (board[3] == player && board[4] == player && board[5] == player) ||
+                (board[6] == player && board[7] == player && board[8] == player) ||
+                (board[0] == player && board[3] == player && board[6] == player) ||
+                (board[1] == player && board[4] == player && board[7] == player) ||
+                (board[2] == player && board[5] == player && board[8] == player) ||
+                (board[0] == player && board[4] == player && board[8] == player) ||
+                (board[2] == player && board[4] == player && board[6] == player)
+                ) {
+                return true;
             } else {
-                fieldElementsM[index].style.color = '#00EAD3'
+                return false;
             }
-           }
+        }
 
-           return {domGameboardM}
+        const humanTurn = () => {
+            fieldElementsM.forEach((field) => {
+                field.addEventListener('click', (e) => {
+                    if(e.target.textContent != '')return;
+ 
+                     // gameControllerM.current(parseInt(e.target.dataset.index))
+                     setGameboardM(parseInt(e.target.dataset.index), 'O'); 
+                     domGameboardM((e.target.dataset.index));
+                     bestMove();
+                                
+                })
+            })
+        }
 
+
+
+        const domGameboardM = (index) => {
+         fieldElementsM[index].textContent = getGameboardM(index);
+         if(getGameboardM(index) === 'X') {
+             fieldElementsM[index].style.color = '#39F01F'
+         } else {
+             fieldElementsM[index].style.color = '#00EAD3'
+         }
+        }
+
+        const resultMessage = (sign) => {
+             if(sign === 'X') {
+                 humanScore.innerText = ''
+                 pointsX++;
+                 currentPlayer.innerText = 'The player has won!'
+                 humanScore.innerText = ': ' + pointsX;
+             }
+             if(sign === 'O') {
+                 machineScore.textContent = '';
+                 pointsO++;
+                 currentPlayer.textContent = 'The machine has won!';
+                 machineScore.textContent = ': ' + pointsO;
+             }
+             if(sign === 'TIE!') {
+                 currentPlayer.textContent = 'TIE!'
+             }
+         }
+
+         bestMove();
+
+        return{setGameboardM, getGameboardM, minimax, bestMove}
+        
        })();
 
-       const gameControllerM = (() => {
-            const fieldElementsM = document.querySelectorAll('.fieldM');
-            let bestScore = -Infinity;
+    //    const displayControllerM = (() => {
 
-            const playRound = (index) => {
-                gameboardM.setGameboardM(index, 'X');
-                displayControllerM.domGameboardM(index);
-                gameboardM.bestMove();
-            }
+    //        const fieldElementsM = document.querySelectorAll('.fieldM');
+    //        const human = document.querySelector('.player');
+    //        const machine = document.querySelector('.machine');
+    //        const humanScore = document.querySelector('.score-player');
+    //        const machineScore = document.querySelector('.score-machine');
+    //        const currentPlayer = document.querySelector('.current-player');
+    //        let pointsX = 0;
+    //        let pointsO = 0;
 
-            // const currentPlayer = () => {
-            //     return round % 2 === 1 ? x.getSign() : bestMove();
-            // }
+    //        const humanTurn = () => {
+    //            fieldElementsM.forEach((field) => {
+    //                field.addEventListener('click', (e) => {
+    //                    if(e.target.textContent != '')return;
+    
+    //                     // gameControllerM.current(parseInt(e.target.dataset.index))
+    //                     gameboardM.setGameboardM(parseInt(e.target.dataset.index), 'O'); 
+    //                     displayControllerM.domGameboardM((e.target.dataset.index));
+    //                     gameboardM.bestMove();
+                                   
+    //                })
+    //            })
+    //        }
 
 
-            return{playRound}
 
-       })();
+    //        const domGameboardM = (index) => {
+    //         fieldElementsM[index].textContent = gameboardM.getGameboardM(index);
+    //         if(gameboardM.getGameboardM(index) === 'X') {
+    //             fieldElementsM[index].style.color = '#39F01F'
+    //         } else {
+    //             fieldElementsM[index].style.color = '#00EAD3'
+    //         }
+    //        }
+
+    //        const resultMessage = (sign) => {
+    //             if(sign === 'X') {
+    //                 humanScore.innerText = ''
+    //                 pointsX++;
+    //                 currentPlayer.innerText = 'The player has won!'
+    //                 humanScore.innerText = ': ' + pointsX;
+    //             }
+    //             if(sign === 'O') {
+    //                 machineScore.textContent = '';
+    //                 pointsO++;
+    //                 currentPlayer.textContent = 'The machine has won!';
+    //                 machineScore.textContent = ': ' + pointsO;
+    //             }
+    //             if(sign === 'TIE!') {
+    //                 currentPlayer.textContent = 'TIE!'
+    //             }
+    //         }
+
+    //        return {domGameboardM, resultMessage, humanTurn}
+
+    //    })();
+
+    //    const gameControllerM = (() => {
+    //         let isOver = false;
+    //         let round = 1;
+
+    //         // const current = (index) => {
+    //         //     if(round % 2 == 1) {
+    //         //         gameboardM.setGameboardM(index, 'X'); 
+    //         //         displayControllerM.domGameboardM(index);
+    //         //         round++
+
+    //         //     }
+    //         //     if(round % 2 == 0) {
+    //         //         gameboardM.bestMove();
+    //         //         round++
+    //         //     }
+    //         // }
+
+    //         // const getIsOverM = () => {
+    //         //     return isOver;
+    //         // }
+
+    //    })();
    }
 
 }
